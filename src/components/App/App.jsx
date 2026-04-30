c68426a4bb3d1e9dd210a586911697dfd46bb792
kloudkitsune <corbansmith92@gmail.com>
2026-04-30 02:09:08 -0400

    Fix: Updated file structure for project.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
